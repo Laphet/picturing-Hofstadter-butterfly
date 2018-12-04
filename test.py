ad1ec2223e5d2abc4e19a96ec94b45811274f384
Laphet <ye_changqing@outlook.com>
2018-12-04 11:25:01 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+from scipy.sparse import diags
+import numpy as np
+from numpy import linalg as LA
+import math 
+import timeit
+
+order = 2**13
+ondiagonal = np.array([2.0*math.cos(2.0* math.pi *j * 0.34) for j in range(order)])
+diagonals = [ondiagonal, np.ones(order-1), np.ones(order-1)]
+tri_mat = diags(diagonals, offsets=[0, 1, -1]).toarray()
+start = timeit.default_timer()
+w = LA.eigvalsh(tri_mat)
+end = timeit.default_timer()
+print('using default QR algorithm in numpy to solve n={:d} tridiagonal matrix consumes time {:f}s\n'.format(order, end-start))
