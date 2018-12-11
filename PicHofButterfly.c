b73df2576f9fa1bcf6aa32befcd4beb8938ad66c
Changqing Ye <yechangqing@lsec.cc.ac.cn>
2018-12-11 17:16:55 +0800

    update

--- PicHofButterfly.c
+++ PicHofButterfly.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 #include "omp.h"
 
 void save_data(FILE* f, int k, int l)
 {
     double* e = (double *)malloc((2 * k + 1) * l * sizeof(double));
     double* off_diag = (double *)malloc(2 * k * sizeof(double));
-    double delta_alpha = 1.0 / (double)l;
+    double delta_alpha = 1.0 / (double)(l + 1);
     int i, j;
 
     for (j = 0; j < l; ++j)
         for (i = -k; i <= k; ++i)
-            e[j * (2 * k + 1) + i + k] = 2.0 * cos(2.0 * M_PI * (double)i * (double)j * delta_alpha);
+            e[j * (2 * k + 1) + i + k] = 2.0 * cos(2.0 * M_PI * (double)i * (double)(j + 1) * delta_alpha);
 
     for (j = 0; j < l; ++j)
     {
@@ -24,12 +25,39 @@ void save_data(FILE* f, int k, int l)
     double alpha;
     for (j = 0; j < l; ++j)
     {
-        alpha = delta_alpha * (double) j;
+        alpha = delta_alpha * (double) (j + 1);
         for (i = 0; i < 2 * k + 1; ++i)
             fprintf(f, "%.8f,%.8f\n", e[j * (2 * k + 1) + i], alpha);
     }
     free(e);
     free(off_diag);
+}
+
+double get_consumed_time(int k, int l)
+{
+    clock_t start, end;
+    double* e = (double *)malloc((2 * k + 1) * l * sizeof(double));
+    double* off_diag = (double *)malloc(2 * k * l * sizeof(double));
+    double delta_alpha = 1.0 / (double) (l + 1);
+    int i, j;
+    for (j = 0; j < l; ++j)
+    {
+        for (i = -k; i <= k; ++i)
+            e[j * (2 * k + 1) + i + k] = 2.0 * cos(2.0 * M_PI * (double)i * (double)(j + 1) * delta_alpha);
+        for (i = 0; i < 2 * k; ++i)
+            off_diag[j * 2 * k + i] = 1.0;
+    }
+    start = clock();
+    #pragma omp parallel for
+    for (j = 0; j < l; ++j)
+    {
+        LAPACKE_dsterf(2 * k + 1, &e[j * (2 * k + 1)], &off_diag[j * 2 * k]);
+
+    }
+    end = clock();
+    free(e);
+    free(off_diag);
+    return (double)(end - start) / CLOCKS_PER_SEC;
 }
 
 int main(int argc, char** argv)
@@ -40,7 +68,14 @@ int main(int argc, char** argv)
         k = atoi(argv[1]);
         l = atoi(argv[2]);
     }
-    FILE *f = fopen("data.csv", "w+");
-    save_data(f, k, l);
-    fclose(f);
+    //FILE *f = fopen("data.csv", "w+");
+    //save_data(f, k, l);
+    //fclose(f);
+    printf("%.6f\n", get_consumed_time(k, l));
+    int num_threads;
+    #pragma omp parallel
+    {
+        num_threads = omp_get_num_threads();
+    }
+    printf("%d\n", num_threads);
 }
